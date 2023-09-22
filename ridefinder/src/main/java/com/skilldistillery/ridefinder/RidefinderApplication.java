bb02a56daea309b108a85bc8b27358d9df5cec11
jrpischel <jrpischel@gmail.com>
2023-09-22 11:34:32 -0500

    Create Boot MVC project, configure project dependency

---
+++ ridefinder/src/main/java/com/skilldistillery/ridefinder/RidefinderApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.ridefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidefinderApplication.class, args);
+	}
+
+}
---
+++ ridefinder/src/test/java/com/skilldistillery/ridefinder/RidefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.ridefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
