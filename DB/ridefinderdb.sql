d5a848eeaae945c61a1787bb4c66a14cb9a08478
Reid Snyder <reidsnyder7@gmail.com>
2023-09-22 19:58:07 -0600

    changed last_update in ride table from TIMESTAMP to DATETIME. Added a bunch of info into db so we can run tests

--- DB/ridefinderdb.sql
+++ DB/ridefinderdb.sql
@@ -116,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `ride` (
   `start_address_id` INT NOT NULL,
   `end_address_id` INT NULL,
   `create_date` DATETIME NULL,
-  `last_update` TIMESTAMP NULL,
+  `last_update` DATETIME NULL,
   `description` TEXT NULL,
   `banner_url` VARCHAR(2000) NULL,
   `skill_level_id` INT NOT NULL,
@@ -373,7 +373,17 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `ridefinderDB`;
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `first_name`, `nickname`, `photo_url`, `description`, `shared`, `last_name`, `address_id`, `create_date`, `last_update`) VALUES (1, 'admin', 'password', 1, NULL, NULL, NULL, NULL, NULL, 1, NULL, 1, NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `first_name`, `nickname`, `photo_url`, `description`, `shared`, `last_name`, `address_id`, `create_date`, `last_update`) VALUES (1, 'admin', 'password', 1, NULL, 'Bill', 'Big Bill', 'https://patch.com/img/cdn/users/255960/2011/10/raw/83577864d6e261e574365a5ea3a0b65e.jpg', 'MC Vet that loves to ride', 1, 'Sanders', 1, '2023/09/22', '2023/09/22');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `club`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `ridefinderDB`;
+INSERT INTO `club` (`id`, `user_id`, `name`, `photo_url`, `enabled`, `description`, `shared`, `create_date`, `last_update`) VALUES (1, 1, 'Brotherhood of Marine Corps Riders', 'https://static.wixstatic.com/media/bd7998_570baecc8624463b9e45d2757615fa51~mv2.jpg/v1/fill/w_938,h_672,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/IMG_6211_JPG.jpg', 1, 'The Brotherhood of Marine Corps Riders (BMCR) is an independent organization and is not affiliated nor sponsored by the United States Marine Corps, any motorcycle manufacturer, or any other organization.​ The BMCR was formed to offer motorcycling camaraderie to active duty, retired, and honorably discharged Marines and Navy “Fleet Marine Forces” (FMF) Corpsmen. The BMCR is not associated with the Confederation of Clubs (COC). We are a motorcycle riding club that share a common love of riding American motorcycles. ', 1, '2023/01/01', '2023/09/22');
 
 COMMIT;
 
@@ -386,6 +396,76 @@ USE `ridefinderDB`;
 INSERT INTO `skill_level` (`id`, `name`, `description`) VALUES (1, 'Beginner', NULL);
 INSERT INTO `skill_level` (`id`, `name`, `description`) VALUES (2, 'Intermediate', NULL);
 INSERT INTO `skill_level` (`id`, `name`, `description`) VALUES (3, 'Advanced', NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `ride`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `ridefinderDB`;
+INSERT INTO `ride` (`id`, `name`, `start_time`, `ride_date`, `duration`, `club_id`, `start_address_id`, `end_address_id`, `create_date`, `last_update`, `description`, `banner_url`, `skill_level_id`, `user_id`, `photo_url`) VALUES (1, 'STREET VIBRATIONS FALL MOTORCYCLE RALLY', '10:00:00', '2023/09/24', '8', 1, 1, 1, '2023/09/22', '2023/09/22', 'STREET VIBRATIONS FALL MOTORCYCLE RALLY', 'https://www.kickstandsup.com/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fkickstandsup-production.appspot.com%2Fo%2Fevent-feed-images%252Fevent-feed-f917a210-6dfc-4e4d-86ee-94ee1801a970%3Falt%3Dmedia%26token%3Da0c68e47-e4a0-4e67-8496-5c1554b02a1d&w=1920&q=75', 1, 1, 'https://www.kickstandsup.com/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fkickstandsup-production.appspot.com%2Fo%2Fevent-feed-images%252Fevent-feed-f917a210-6dfc-4e4d-86ee-94ee1801a970%3Falt%3Dmedia%26token%3Da0c68e47-e4a0-4e67-8496-5c1554b02a1d&w=1920&q=75');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `ride_comment`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `ridefinderDB`;
+INSERT INTO `ride_comment` (`id`, `ride_id`, `user_id`, `comment`, `picture_url`, `enabled`, `create_date`, `inreply_id`) VALUES (1, 1, 1, 'What a blast', 'https://riderjustice.com/wp-content/uploads/2023/04/EventHeader.jpg', 1, '2023/09/22', 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `club_member`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `ridefinderDB`;
+INSERT INTO `club_member` (`user_id`, `club_id`, `administrator`, `date_joined`, `nickname`) VALUES (1, 1, NULL, NULL, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `ride_photo`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `ridefinderDB`;
+INSERT INTO `ride_photo` (`id`, `photo_url`, `create_date`, `caption`, `ride_id`, `user_id`, `enabled`) VALUES (1, 'https://d56b293rhv8dp.cloudfront.net/events/3/images/medium_w/er-events_0016_Bikes__Blues__and_BBQ.jpg?1570040611', '2023/09/22', 'Lets Ride', 1, 1, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `club_comment`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `ridefinderDB`;
+INSERT INTO `club_comment` (`id`, `user_id`, `comment`, `picture_url`, `enabled`, `create_date`, `inreply_id`, `club_id`) VALUES (1, 1, 'Lets go for a ride', 'https://cloudfront-us-east-1.images.arcpublishing.com/archetype/3UIW3CSEJZDFPPPRMPJSS4KRFM.jpg', 1, '2023/09/22', NULL, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `user_comment`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `ridefinderDB`;
+INSERT INTO `user_comment` (`id`, `user_id`, `comment`, `picture_url`, `enabled`, `create_date`, `inreply_id`, `recipient_id`, `shared`) VALUES (1, 1, 'Check this out!', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpatch.com%2Fcalifornia%2Fbanning-beaumont%2Fvietnam-vet-bikers-disrespected-at-casino-tribal-rep-3021b82015&psig=AOvVaw1Oa5ceQMaaEK3IiP5A3x_G&ust=1695515810419000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCLiivt6-v4EDFQAAAAAdAAAAABAJ', 1, '2023/09/22', NULL, 1, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `rider`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `ridefinderDB`;
+INSERT INTO `rider` (`user_id`, `ride_id`, `rating`, `comment`, `create_date`) VALUES (1, 1, 5, 'Was great, would do it again!', '2023/09/22');
 
 COMMIT;
 
