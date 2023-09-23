84addf94f6d12283b4cdb663dd16383d8d3cd184
cas-e <caseyk6.28@gmail.com>
2023-09-23 06:27:17 -0700

    add clubComment entity and test

--- JPARideFinder/src/main/java/com/skilldistillery/ridefinder/entities/ClubComment.java
+++ JPARideFinder/src/main/java/com/skilldistillery/ridefinder/entities/ClubComment.java
@@ -8,128 +8,91 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-public class Club {
+@Table(name = "club_comment")
+public class ClubComment {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
-	
 //	TODO foreign key
 //	@Column(name = "user_id")
-//	private int userId;	
+//	private int userId;
 	
+	private String comment;
 	
-	private String name;
-	
-	@Column(name = "photo_url")
-	private String photoUrl;
+	@Column(name = "picture_url")
+	private String pictureUrl;
 	
 	private boolean enabled;
-	
-	private String description;
-	
-	private boolean shared;
 	
 	@Column(name = "create_date")
 	private LocalDateTime createDate;
 	
-	@Column(name = "last_update")
-	private LocalDateTime lastUpdate;
+	@Column(name = "inreply_id")
+	private Integer inreplyId;
+
+//	TODO foreign key
+//	@Column(name = "club_id")
+//	private int clubId;
 	
 	
-	public Club() {}
-
-
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
-
-
-	public String getName() {
-		return name;
+	public String getComment() {
+		return comment;
 	}
 
-
-	public void setName(String name) {
-		this.name = name;
+	public void setComment(String comment) {
+		this.comment = comment;
 	}
 
-
-	public String getPhotoUrl() {
-		return photoUrl;
+	public String getPictureUrl() {
+		return pictureUrl;
 	}
 
-
-	public void setPhotoUrl(String photoUrl) {
-		this.photoUrl = photoUrl;
+	public void setPictureUrl(String pictureUrl) {
+		this.pictureUrl = pictureUrl;
 	}
-
 
 	public boolean isEnabled() {
 		return enabled;
 	}
 
-
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
-
-
-	public String getDescription() {
-		return description;
-	}
-
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-
-	public boolean isShared() {
-		return shared;
-	}
-
-
-	public void setShared(boolean shared) {
-		this.shared = shared;
-	}
-
 
 	public LocalDateTime getCreateDate() {
 		return createDate;
 	}
 
-
 	public void setCreateDate(LocalDateTime createDate) {
 		this.createDate = createDate;
 	}
 
-
-	public LocalDateTime getLastUpdate() {
-		return lastUpdate;
+	public Integer getInreplyId() {
+		return inreplyId;
 	}
 
-
-	public void setLastUpdate(LocalDateTime lastUpdate) {
-		this.lastUpdate = lastUpdate;
+	public void setInreplyId(Integer inreplyId) {
+		this.inreplyId = inreplyId;
 	}
 
-
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -139,18 +102,17 @@ public class Club {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Club other = (Club) obj;
+		ClubComment other = (ClubComment) obj;
 		return id == other.id;
 	}
 
-
+	
 	@Override
 	public String toString() {
-		return "Club [id=" + id + ", name=" + name + ", photoUrl=" + photoUrl + ", enabled=" + enabled
-				+ ", description=" + description + ", shared=" + shared + ", createDate=" + createDate + ", lastUpdate="
-				+ lastUpdate + "]";
-	}
-	
+		return "ClubContent [id=" + id + ", comment=" + comment + ", pictureUrl=" + pictureUrl + ", enabled=" + enabled
+				+ ", createDate=" + createDate + ", inreplyId=" + inreplyId + "]";
+	}  
 	
 	
 }
+
